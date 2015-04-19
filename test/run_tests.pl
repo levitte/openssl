0f1699e6024952b8adfd682ce0b125b710a6f7e7
Richard Levitte <levitte@openssl.org>
2015-04-19 23:44:58 +0200

    Allow the test harness to take arguments
    It's been possible all along to run just some of the tests by
    modifying the make variable TESTS.  To allow that to continue,
    run_tests.pl needs to take arguments.

--- test/run_tests.pl
+++ test/run_tests.pl
@@ -5,4 +5,18 @@ use warnings;
 
 use Test::Harness;
 
-runtests(<recipes/*.t>);
+my @tests = ( "alltests" );
+if (@ARGV) {
+    @tests = @ARGV;
+}
+if (grep /^alltests$/, @tests) {
+    @tests = <recipes/[0-9][0-9]-*.t>;
+} else {
+    my @t = ();
+    foreach (@tests) {
+	push @t, <recipes/[0-9][0-9]-$_.t>;
+    }
+    @tests = @t;
+}
+
+runtests(sort @tests);
