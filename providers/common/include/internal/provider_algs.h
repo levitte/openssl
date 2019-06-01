93a3616e856d4dba022ee2ad52baf9c7b8f50bfe
Richard Levitte <levitte@openssl.org>
2019-06-01 11:18:15 +0200

    Move CMAC to providers

--- providers/common/include/internal/provider_algs.h
+++ providers/common/include/internal/provider_algs.h
@@ -61,6 +61,7 @@ extern const OSSL_DISPATCH aes128ctr_functions[];
 /* MACs */
 extern const OSSL_DISPATCH blake2bmac_functions[];
 extern const OSSL_DISPATCH blake2smac_functions[];
+extern const OSSL_DISPATCH cmac_functions[];
 
 /* Key management */
 extern const OSSL_DISPATCH dh_keymgmt_functions[];
