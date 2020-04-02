ca6da8191838bbf0c6774e7f84b9bcf1ad2dcb60
Richard Levitte <levitte@openssl.org>
2020-04-02 23:52:10 +0200

    squash! PROV: Add the beginning of a DER writing library
    The DER writing library is now based on WPACKET.  The example needs
    another rewrite:
    
        /*
         * Fill in this structure:
         *
         * something ::= SEQUENCE {
         *     id OBJECT IDENTIFIER,
         *     x [0] INTEGER OPTIONAL,
         *     y [1] BOOLEAN OPTIONAL,
         *     n INTEGER
         * }
         */
        unsigned char buf[nnnn], *p = NULL;
        size_t encoded_len = 0;
        WPACKET pkt;
    
        return WPACKET_init_der(&pkt, buf, sizeof(buf)
            && DER_w_start_sequence(&pkt, -1)
            && DER_w_bn(&pkt, -1, bn)
            && DER_w_boolean(&pkt, 1, bool)
            && DER_w_precompiled(&pkt, -1, OID, sizeof(OID))
            && DER_w_end_sequence(&pkt, -1)
            && WPACKET_finish(&pkt)
            && WPACKET_get_total_written(&pkt, &encoded_len)
            && (p = WPACKET_get_curr(&pkt)) != NULL;

---
+++ crypto/der_writer.c
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2020 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include "internal/cryptlib.h"
+#include "internal/der.h"
+#include "crypto/bn.h"
+
+static int int_write_bytes(WPACKET *pkt,
+                           const unsigned char *bytes, size_t bytes_n)
+{
+    unsigned char *p = NULL;
+
+    if (!WPACKET_allocate_bytes(pkt, bytes_n, &p))
+        return 0;
+    if (p != NULL)
+        memcpy(p, bytes, bytes_n);
+    return 1;
+}
+
+static int int_start_context(WPACKET *pkt, int cont)
+{
+    if (cont < 0)
+        return 1;
+    return WPACKET_start_sub_packet(pkt);
+}
+
+static int int_end_context(WPACKET *pkt, int cont)
+{
+    if (cont < 0)
+        return 1;
+    return WPACKET_close(pkt)
+        && WPACKET_put_bytes_u8(pkt, DER_C_CONTEXT|cont);
+}
+
+int DER_w_precompiled(WPACKET *pkt, int cont,
+                      const unsigned char *precompiled, size_t precompiled_n)
+{
+    return int_start_context(pkt, cont)
+        && int_write_bytes(pkt, precompiled, precompiled_n)
+        && int_end_context(pkt, cont);
+}
+
+int DER_w_boolean(WPACKET *pkt, int cont, int b)
+{
+    return int_start_context(pkt, cont)
+        && WPACKET_start_sub_packet(pkt)
+        && (!b || WPACKET_put_bytes_u8(pkt, 0xFF))
+        && !WPACKET_close(pkt)
+        && !WPACKET_put_bytes_u8(pkt, DER_P_BOOLEAN)
+        && int_end_context(pkt, cont);
+}
+
+/* For integers, we only support unsigned values for now */
+int DER_w_ulong(WPACKET *pkt, int cont, unsigned long v)
+{
+    size_t n = 0;
+    unsigned long tmp = v;
+
+    while (tmp != 0) {
+        n++;
+        tmp >>= 8;
+    }
+    if (n == 0)
+        n = 1;
+
+    return int_start_context(pkt, cont)
+        && WPACKET_start_sub_packet(pkt)
+        && WPACKET_put_bytes__(pkt, v, n)
+        && WPACKET_close(pkt)
+        && WPACKET_put_bytes_u8(pkt, DER_P_INTEGER)
+        && int_end_context(pkt, cont);
+}
+
+int DER_w_bn(WPACKET *pkt, int cont, const BIGNUM *v)
+{
+    size_t n = 0;
+    unsigned int top_byte;
+    unsigned char *p = NULL;
+
+    if (v == NULL || BN_is_negative(v))
+        return 0;
+    if (BN_is_zero(v))
+        return DER_w_ulong(pkt, cont, 0);
+
+    /* The BIGNUM limbs are in LE order */
+    n = BN_num_bytes(v);
+    top_byte =
+        ((bn_get_words(v) [(n - 1) / BN_BYTES]) >> (8 * ((n - 1) % BN_BYTES)))
+        & 0xFF;
+    if (top_byte > 0x7F)
+        n++;
+
+    if (!int_start_context(pkt, cont)
+        || !WPACKET_start_sub_packet(pkt)
+        || !WPACKET_allocate_bytes(pkt, n, &p))
+        return 0;
+
+    if (p != NULL) {
+        BN_bn2binpad(v, p, n);
+
+        /* Double check that we got the top byte correctly */
+        if (top_byte > 0x7F && !ossl_assert(top_byte == p[1]))
+            return 0;
+    }
+
+    return WPACKET_close(pkt)
+        && WPACKET_put_bytes_u8(pkt, DER_P_INTEGER)
+        && int_end_context(pkt, cont);
+}
+
+int DER_w_null(WPACKET *pkt, int cont)
+{
+    return int_start_context(pkt, cont)
+        && WPACKET_start_sub_packet(pkt)
+        && WPACKET_close(pkt)
+        && WPACKET_put_bytes_u8(pkt, DER_P_INTEGER)
+        && int_end_context(pkt, cont);
+}
+
+/* Constructed things need a start and an end */
+int DER_w_start_sequence(WPACKET *pkt, int cont)
+{
+    return int_start_context(pkt, cont)
+        && WPACKET_start_sub_packet(pkt);
+}
+
+int DER_w_end_sequence(WPACKET *pkt, int cont)
+{
+    return WPACKET_close(pkt)
+        && WPACKET_put_bytes_u8(pkt, DER_F_CONSTRUCTED|DER_P_SEQUENCE)
+        && int_end_context(pkt, cont);
+}
--- include/internal/der.h
+++ include/internal/der.h
@@ -8,9 +8,20 @@
  */
 
 #include <openssl/bn.h>
+#include "internal/packet.h"
+
+/*
+ * NOTE: X.690 numbers the identifier octet bits 1 to 8.
+ * We use the same numbering in comments here.
+ */
 
 /* Well known primitive tags */
 
+/*
+ * DER UNIVERSAL tags, occupying bits 1-5 in the DER identifier byte
+ * These are only valid for the UNIVERSAL class.  With the other classes,
+ * these bits have a different meaning.
+ */
 #define DER_P_EOC                       0 /* BER End Of Contents tag */
 #define DER_P_BOOLEAN                   1
 #define DER_P_INTEGER                   2
@@ -38,9 +49,11 @@
 #define DER_P_UNIVERSALSTRING          28
 #define DER_P_BMPSTRING                30
 
+/* DER Flags, occupying bit 6 in the DER identifier byte */
 #define DER_F_PRIMITIVE              0x00
 #define DER_F_CONSTRUCTED            0x20
 
+/* DER classes tags, occupying bits 7-8 in the DER identifier byte */
 #define DER_C_UNIVERSAL              0x00
 #define DER_C_APPLICATION            0x40
 #define DER_C_CONTEXT                0x80
@@ -53,6 +66,25 @@
  * that way.
  */
 
+/* This can be used for all items that don't have a context */
+#define DER_NO_CONTEXT  -1
+
+int DER_w_precompiled(WPACKET *pkt, int cont,
+                      const unsigned char *precompiled, size_t precompiled_n);
+
+int DER_w_boolean(WPACKET *pkt, int cont, int b);
+int DER_w_ulong(WPACKET *pkt, int cont, unsigned long v);
+int DER_w_bn(WPACKET *pkt, int cont, const BIGNUM *v);
+int DER_w_null(WPACKET *pkt, int cont);
+
+/*
+ * All constructors for constructed elements have a start and a stop function
+ */
+int DER_w_start_sequence(WPACKET *pkt, int cont);
+int DER_w_end_sequence(WPACKET *pkt, int cont);
+
+
+
 #if 0                        /* Example code should not be compiled */
 
 /*
@@ -60,55 +92,46 @@
  * To build the RSASSA-PSS AlgorithmIndentifier with the restrictions
  * hashAlgorithm = SHA256, maskGenAlgorithm = mgf1SHA256, saltLength = 20,
  * this is the expected code:
+ *
+ * Reminder:
+ * RSASSA-PSS-params ::= SEQUENCE {
+ *     hashAlgorithm      [0] HashAlgorithm      DEFAULT sha1,
+ *     maskGenAlgorithm   [1] MaskGenAlgorithm   DEFAULT mgf1SHA1,
+ *     saltLength         [2] INTEGER            DEFAULT 20,
+ *     trailerField       [3] TrailerField       DEFAULT trailerFieldBC
+ * }
  */
 
 const unsigned char der_oid_sha256[N]; /* N is to be determined */
 
 /* buf is the buffer we write the DER content into, sz is its size */
-unsigned char *p = buf + sz;
-const unsigned char *start = buf; /* For boundary checking */
-size_t length = 0;
-size_t c2_length = 0;
-size_t c1_length = 0;
-size_t c0_length = 0;
+WPACKET pkt;
 
-if (!DER_w_ulong(&p, &c2_length, start, 20)
-    || !DER_w_context(&p, &c2_length, start, 2)
-    /* ... */
-    || !DER_w_precompiled(&p, &c1_length, start,
-                          der_oid_mgf1, sizeof(der_oid_mgf1))
-    || !DER_w_context(&p, &c1_length, start, 1)
-    /* ... */
-    || !DER_w_precompiled(&p, &c0_length, start,
-                          der_oid_sha256, sizeof(der_oid_sha256))
-    || !DER_w_context(&p, &c0_length, start, 0)
-    /* ... */
-    || !DER_w_sequence_n(&p, &length, start, c0_length + c1_length + c2_length)
-    || !DER_w_precompiled(&p, &length, start,
-                          der_oid_rsassaPss, sizeof(der_oid_rsassaPss))
-    || !DER_w_sequence(&p, &length, start))
+unsigned char *p = buf + sz;
+
+if (!WPACKET_init_der(&pkt, buf, sz)
+    /* AlgorithmIdentifier SEQUENCE subpacket */
+    || (!DER_w_start_sequence(&pkt, DER_NO_CONTEXT)
+        /* Parameter subpacket */
+        || (!DER_w_start_sequence(&pkt, DER_NO_CONTEXT)
+            /* Context 2 */
+            || !DER_w_ulong(&pkt, 2, 20)
+            /* Context 1 */
+            || !DER_w_precompiled(&pkt, 1,
+                                  der_oid_mgf1, sizeof(der_oid_mgf1))
+            /* Context 0 */
+            || !DER_w_precompiled(&pkt, 0,
+                                  der_oid_sha256, sizeof(der_oid_sha256))
+            || !DER_w_end_sequence(&pkt, DER_NO_CONTEXT))
+        || !DER_w_precompiled(&pkt, DER_NO_CONTEXT,
+                              der_oid_rsassaPss, sizeof(der_oid_rsassaPss))
+        || !DER_w_end_sequence(&pkt, DER_NO_CONTEXT))
+    || !WPACKET_finish(&pkt)
+    || !WPACKET_get_total_written(&pkt, &length)
+    || (p = WPACKET_get_curr(&pkt)) == NULL)
     /* ERROR */ ;
 
 /* At this point, |p| is the start of the DER blob and |length| is its length */
 
 #endif
 
-int DER_w_precompiled(unsigned char **pp, size_t *cnt,
-                      const unsigned char *start,
-                      const unsigned char *precompiled, size_t precompiled_n);
-
-int DER_w_boolean(unsigned char **pp, size_t *cnt, const unsigned char *start,
-                  int b);
-int DER_w_ulong(unsigned char **pp, size_t *cnt, const unsigned char *start,
-                unsigned long v);
-int DER_w_bn(unsigned char **pp, size_t *cnt, const unsigned char *start,
-                BIGNUM *v);
-int DER_w_null(unsigned char **pp, size_t *cnt, const unsigned char *start);
-int DER_w_sequence(unsigned char **pp, size_t *cnt, const unsigned char *start);
-int DER_w_sequence_n(unsigned char **pp, size_t *cnt,
-                     const unsigned char *start,
-                     size_t n);
-int DER_w_context(unsigned char **pp, size_t *cnt, const unsigned char *start,
-                  int num);
-int DER_w_context_n(unsigned char **pp, size_t *cnt, const unsigned char *start,
-                    int num, size_t n);
