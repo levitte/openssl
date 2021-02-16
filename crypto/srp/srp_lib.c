80c82b831c39e10da8e2c899402cd8382a95cbdf
Matt Caswell <matt@openssl.org>
2021-02-16 15:08:34 +0000

    Update copyright year
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- crypto/srp/srp_lib.c
+++ crypto/srp/srp_lib.c
@@ -5,7 +5,7 @@
  * EdelKey project and contributed to the OpenSSL project 2004.
  */
 /* ====================================================================
- * Copyright (c) 2004 The OpenSSL Project.  All rights reserved.
+ * Copyright (c) 2004-2021 The OpenSSL Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
