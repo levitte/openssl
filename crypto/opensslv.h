2007deda04927d3444137ac53f3600030258f328
Matt Caswell <matt@openssl.org>
2021-08-24 14:40:17 +0100

    Prepare for 1.0.2zb-dev
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -30,11 +30,11 @@ extern "C" {
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-# define OPENSSL_VERSION_NUMBER  0x100021afL
+# define OPENSSL_VERSION_NUMBER  0x100021b0L
 # ifdef OPENSSL_FIPS
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2za-fips  24 Aug 2021"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2zb-fips-dev  xx XXX xxxx"
 # else
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2za  24 Aug 2021"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2zb-dev  xx XXX xxxx"
 # endif
 # define OPENSSL_VERSION_PTEXT   " part of " OPENSSL_VERSION_TEXT
 
