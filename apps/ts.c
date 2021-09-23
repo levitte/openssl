b1dd44e7c274ebc372b7ef07136729edd999df3b
Matt Caswell <matt@openssl.org>
2021-09-23 14:22:50 +0100

    Update copyright year
    Reviewed-by: Tomas Mraz <tomas@openssl.org>

--- apps/ts.c
+++ apps/ts.c
@@ -4,7 +4,7 @@
  * 2002.
  */
 /* ====================================================================
- * Copyright (c) 2001 The OpenSSL Project.  All rights reserved.
+ * Copyright (c) 2001-2021 The OpenSSL Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
