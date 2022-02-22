9f0ffce0834c7f7ff4e6daad8314d4876ee5c109
Matt Caswell <matt@openssl.org>
2022-02-22 14:19:43 +0000

    Update copyright year
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>

--- crypto/ec/ec.h
+++ crypto/ec/ec.h
@@ -7,7 +7,7 @@
  * \author Originally written by Bodo Moeller for the OpenSSL project
  */
 /* ====================================================================
- * Copyright (c) 1998-2020 The OpenSSL Project.  All rights reserved.
+ * Copyright (c) 1998-2022 The OpenSSL Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
