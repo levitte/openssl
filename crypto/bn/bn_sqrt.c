c39e9661090dbddf99b9f1a67aa39f953ee7eea1
Matt Caswell <matt@openssl.org>
2022-03-15 14:00:59 +0000

    Update copyright year
    Reviewed-by: Tim Hudson <tjh@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>

--- crypto/bn/bn_sqrt.c
+++ crypto/bn/bn_sqrt.c
@@ -4,7 +4,7 @@
  * Moeller for the OpenSSL project.
  */
 /* ====================================================================
- * Copyright (c) 1998-2000 The OpenSSL Project.  All rights reserved.
+ * Copyright (c) 1998-2022 The OpenSSL Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
