b000c861cc05bb3bb58bccee1da53cbd6d8e8624
Richard Levitte <levitte@openssl.org>
2023-02-07 12:50:05 +0100

    Update copyright year
    Release: yes
    Reviewed-by: Tomas Mraz <tomas@openssl.org>

--- test/cms-test.pl
+++ test/cms-test.pl
@@ -3,7 +3,7 @@
 # project.
 #
 # ====================================================================
-# Copyright (c) 2008-2018 The OpenSSL Project.  All rights reserved.
+# Copyright (c) 2008-2023 The OpenSSL Project.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
