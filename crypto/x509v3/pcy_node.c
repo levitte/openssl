2dcd4f1e3115f38cefa43e3efbe9b801c27e642e
Pauli <pauli@openssl.org>
2023-03-08 15:28:20 +1100

    x509: excessive resource use verifying policy constraints
    A security vulnerability has been identified in all supported versions
    of OpenSSL related to the verification of X.509 certificate chains
    that include policy constraints.  Attackers may be able to exploit this
    vulnerability by creating a malicious certificate chain that triggers
    exponential use of computational resources, leading to a denial-of-service
    (DoS) attack on affected systems.
    
    Fixes CVE-2023-0464
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>

--- crypto/x509v3/pcy_node.c
+++ crypto/x509v3/pcy_node.c
@@ -111,9 +111,15 @@ X509_POLICY_NODE *level_find_node(const X509_POLICY_LEVEL *level,
 X509_POLICY_NODE *level_add_node(X509_POLICY_LEVEL *level,
                                  const X509_POLICY_DATA *data,
                                  X509_POLICY_NODE *parent,
-                                 X509_POLICY_TREE *tree)
+                                 X509_POLICY_TREE *tree,
+                                 int extra_data)
 {
     X509_POLICY_NODE *node;
+
+    /* Verify that the tree isn't too large.  This mitigates CVE-2023-0464 */
+    if (tree->node_maximum > 0 && tree->node_count >= tree->node_maximum)
+        return NULL;
+
     node = OPENSSL_malloc(sizeof(X509_POLICY_NODE));
     if (!node)
         return NULL;
@@ -136,7 +142,7 @@ X509_POLICY_NODE *level_add_node(X509_POLICY_LEVEL *level,
         }
     }
 
-    if (tree) {
+    if (extra_data) {
         if (!tree->extra_data)
             tree->extra_data = sk_X509_POLICY_DATA_new_null();
         if (!tree->extra_data)
@@ -145,6 +151,7 @@ X509_POLICY_NODE *level_add_node(X509_POLICY_LEVEL *level,
             goto node_error;
     }
 
+    tree->node_count++;
     if (parent)
         parent->nchild++;
 
